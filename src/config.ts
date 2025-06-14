2bbeaed59d9c319f2099afe55a370d47a730bc0c
hxsyzl <knbn666@gmail.com>
2025-06-14 19:14:33 +0800

    添加站点统计

--- src/config.ts
+++ src/config.ts
@@ -45,8 +45,8 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     LinkPreset.Friends, 
     {
-      name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
+      name: '站点统计',
+      url: 'https://cloud.umami.is/share/c0bVr68LvRzdqCQQ/wuxinrom.xyz',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
